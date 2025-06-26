8b39296422605f83ea7c42c1ede6b41684642c1c
hanynan <hanynan8@gmail.com>
2025-06-26 22:46:35 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-// import { useEffect } from 'react';
 import Navbar from './component/navbar';
 import Home from './view/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import  Create  from "./view/Create";
-import  BlogDetails  from "./view/BlogDetaails";
+import { Route, Routes } from 'react-router-dom'; // ✅ بدون BrowserRouter هنا
+import Create from './view/Create';
+import BlogDetails from './view/BlogDetaails';
 import ErrorPage from './view/Error404';
 
 function App() {
   return (
+    <div className="App">
+      <Navbar />
 
-    <Router>
-      <div className="App">
-        <Navbar />
-
-        <div className="content">
-          <Routes>
-            <Route path='/' element={<Home />} /> 
-            <Route path='/create' element={<Create />} /> 
-            <Route path='/blog/:id' element={<BlogDetails />} /> 
-            <Route path='*' element={<ErrorPage />} /> 
-          </Routes>
-        </div>
-        
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/blog/:id" element={<BlogDetails />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </div>
-    </Router>
-
+    </div>
   );
 }
 
